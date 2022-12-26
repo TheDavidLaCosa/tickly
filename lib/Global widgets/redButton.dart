262c5437f7bbef5fa52ecc9d48926b0448a84f62
David Larrosa <davidlarrosacamps@gmail.com>
2022-12-26 09:02:36 +0100

    Botons vermells dissenyats

---
+++ lib/Global widgets/redButton.dart
@@ -0,0 +1,40 @@
+///
+/// NOTA: No posar textos molt llargs que si no la caixa es fa massa gran.
+///
+
+import 'package:flutter/cupertino.dart';
+
+class redButton extends StatelessWidget {
+
+  final String text;
+
+  const redButton({Key? key,
+                  required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(210, 36, 36, 1),
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(),
+      ),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            width: 300,
+            height: 50,
+            //padding: const EdgeInsets.fromLTRB(110, 10, 110, 10),
+            child: Center(
+              child: Text(text, style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w300, color: Color.fromRGBO(
+                255, 255, 255, 1.0))),
+            ),
+          ),
+
+        ]
+      ),
+    );
+  }
+}
