4afbeae8fcc6636c3943410c8d17db1f7734681f
David Larrosa <davidlarrosacamps@gmail.com>
2022-12-26 09:38:27 +0100

    Els botons vermells tenen listeners (on click)

--- lib/Global widgets/redButton.dart
+++ lib/Global widgets/redButton.dart
@@ -3,6 +3,7 @@
 ///
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class redButton extends StatelessWidget {
 
@@ -11,29 +12,37 @@ class redButton extends StatelessWidget {
   const redButton({Key? key,
                   required this.text}) : super(key: key);
 
+  //Aquesta el nom és temporal, la funció es fa servir per veure que funciona el GestureDetector
+  void doSomething(){
+    print("AAA");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(210, 36, 36, 1),
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(),
-      ),
+    return GestureDetector(
+      onTap: doSomething,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(210, 36, 36, 1),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(),
+        ),
 
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            width: 300,
-            height: 50,
-            //padding: const EdgeInsets.fromLTRB(110, 10, 110, 10),
-            child: Center(
-              child: Text(text, style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w300, color: Color.fromRGBO(
-                255, 255, 255, 1.0))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              width: 300,
+              height: 50,
+              //padding: const EdgeInsets.fromLTRB(110, 10, 110, 10),
+              child: Center(
+                child: Text(text, style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w300, color: Color.fromRGBO(
+                  255, 255, 255, 1.0))),
+              ),
             ),
-          ),
 
-        ]
+          ]
+        ),
       ),
     );
   }
